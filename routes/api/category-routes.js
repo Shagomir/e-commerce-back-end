f00d6ec26b7fff9aa54629f275ccd5e2e02e4144
Shagomir <m.j.knudson@gmail.com>
2023-12-07 14:08:04 -0600

    updated models and routes

---
+++ routes/api/category-routes.js
@@ -0,0 +1,44 @@
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
+
+// The `/api/categories` endpoint
+
+router.get("/", async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  // find all categories
+  // be sure to include its associated Products
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  // find one category by its `id` value
+  // be sure to include its associated Products
+});
+
+router.post("/", async (req, res) => {
+  // create a new category
+});
+
+router.put("/:id", async (req, res) => {
+  // update a category by its `id` value
+});
+
+router.delete("/:id", async (req, res) => {
+  // delete a category by its `id` value
+});
+
+module.exports = router;
